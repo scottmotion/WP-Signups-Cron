b1099050033e10b7320bf7b95473ab8781dcc9d0
scottmotion <scott@scottmotion.com>
2024-12-07 15:13:34 -0800

    Update class-signups-cron-activator.php

--- signups-cron/includes/class-signups-cron-activator.php
+++ signups-cron/includes/class-signups-cron-activator.php
@@ -23,6 +23,28 @@
 class Signups_Cron_Activator {
 
 	/**
+	 * The array of default options set during plugin activation.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      array    $default_options    The array of default options set during plugin activation.
+	 */
+	// protected $default_options;
+
+	/**
+	 * Initialize the collection used to set the default options during plugin activation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+
+		// $this->default_options = array(
+		// 	'__construct key'      => '__construct value'
+		// );
+
+	}
+
+	/**
 	 * Short Description. (use period)
 	 *
 	 * Long Description.
@@ -30,6 +52,20 @@ class Signups_Cron_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+
+		add_option(
+			'signups_cron_settings',				// Name of the option to add.
+			array(									// Option value. 
+				'active_enabled'		=> false,
+				'active_threshold'		=> 365,
+				'pending_enabled'		=> false,
+				'pending_threshold'		=> 365,
+				'send_email_report'		=> true,
+				'cron_schedule'			=> 'daily'
+			),
+			'',										// Description (Deprecated).
+			'off'									// Whether to load the option when WordPress starts up.
+		);
 
 	}
 
