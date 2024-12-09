ef34aac1a68b864fa83bf61a8e89d82600bb7ea5
scottmotion <scott@scottmotion.com>
2024-12-08 16:50:56 -0800

    Update class-signups-cron-admin.php

--- signups-cron/admin/class-signups-cron-admin.php
+++ signups-cron/admin/class-signups-cron-admin.php
@@ -315,19 +315,19 @@ class Signups_Cron_Admin {
 		<table class="signups-table-info">
 			<tr>
                 <td>Signups Table Size:</td>
-                <td><?php number_format( $data["signups_table_size"], 2, '.' ); ?> MB</td>
+                <td><?php echo number_format( $data["signups_table_size"], 2, '.' ); ?> MB</td>
             </tr>
             <tr>
                 <td>Total Signups Count:</td>
-                <td><?php number_format( $data["signups_count_total"] ); ?></td>
+                <td><?php echo number_format( $data["signups_count_total"] ); ?></td>
             </tr>
             <tr>
                 <td>Active Signups Count:</td>
-                <td><?php number_format( $data["signups_count_active"] ); ?></td>
+                <td><?php echo number_format( $data["signups_count_active"] ); ?></td>
             </tr>
             <tr>
                 <td>Pending Signups Count:</td>
-                <td><?php number_format( $data["signups_count_pending"] ); ?></td>
+                <td><?php echo number_format( $data["signups_count_pending"] ); ?></td>
             </tr>
 		</table>
 		<?php
