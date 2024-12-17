cddc4a9ae387546acae6416c64f06ff3b94b0b25
scottmotion <scott@scottmotion.com>
2024-12-17 11:59:41 -0800

    Update class-signups-cron-activator.php

--- signups-cron/includes/class-signups-cron-activator.php
+++ signups-cron/includes/class-signups-cron-activator.php
@@ -29,33 +29,12 @@ class Signups_Cron_Activator {
 	 */
 	public static function activate() {
 
-		/**
-		 * TODO:
-		 * [] set option defaults in Signups_Cron_Admin. Just add empty option here?
-		 * [?] Do we need to set empty ('0') values?
-		 * [?] Check if option exists because plugin was deactivated but not uninstalled?
-		 */
-		// add_option(
-		// 	'signups_cron_settings',				// Name of the option to add.
-		// 	array(									// Option value. 
-		// 		'signups_cron_field_active_enabled'			=> '0',		// Active Signups cron enabled.
-		// 		'signups_cron_field_active_threshold'		=> '365',	// Active Signups cron threshold.
-		// 		'signups_cron_field_pending_enabled'		=> '0',		// Pending Signups cron enabled.
-		// 		'signups_cron_field_pending_threshold'		=> '365',	// Pending Signups cron threshold.
-		// 		'signups_cron_field_send_email_report'		=> '0',		// Send cron email report enabled.
-		// 		'signups_cron_field_cron_schedule'			=> 'daily'	// Cron schedule recurrence.
-		// 	),
-		// 	'',										// Description (Deprecated).
-		// 	'off'									// Whether to load the option when WordPress starts up.
-		// );
-
 		add_option(
 			'signups_cron_settings',	// Name of the option to add.
 			'',							// Option value. 
 			'',							// Description (Deprecated).
 			'off'						// Whether to load the option when WordPress starts up.
 		);
-
 
 	}
 
