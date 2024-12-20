c7e8c1e56af6041054f7d1c70fe0451810743d4c
scottmotion <scott@scottmotion.com>
2024-12-19 19:44:53 -0800

    Update class-signups-cron-table-info.php
    wpdb prepare

--- signups-cron/admin/class-signups-cron-table-info.php
+++ signups-cron/admin/class-signups-cron-table-info.php
@@ -19,7 +19,7 @@ class Signups_Cron_Table_Info {
 	 * @access private
 	 * @var string $table_name The name of the database table for signups.
 	 */
-	private $table_name;
+	// private $table_name;
 
 	/**
 	 * Initialize the class and set its properties.
@@ -39,18 +39,17 @@ class Signups_Cron_Table_Info {
     public function get_signups_table_info() {
 
         global $wpdb;
-        $table_name = esc_sql( $wpdb->prefix . 'signups' ); // Sanitize the table name.
-
+        $table_name = $wpdb->prefix . 'signups';
+        
         $signups_table_info = [];
     
-        // $signups_table_info["signups_count_total"] = $wpdb->get_var( "SELECT COUNT(*) FROM {$table_name}" );
-        $signups_table_info["signups_count_active"] = $wpdb->get_var( "SELECT COUNT(*) FROM {$table_name} WHERE active = 1" );
-        $signups_table_info["signups_count_pending"] = $wpdb->get_var( "SELECT COUNT(*) FROM {$table_name} WHERE active = 0" );
+        $signups_table_info["signups_count_active"] = $wpdb->get_var( $wpdb->prepare("SELECT COUNT(*) FROM %i WHERE active = %d", $table_name, 1) );
+        $signups_table_info["signups_count_pending"] = $wpdb->get_var( $wpdb->prepare("SELECT COUNT(*) FROM %i WHERE active = %d", $table_name, 0) );
         $signups_table_info["signups_count_total"] = $signups_table_info["signups_count_active"] + $signups_table_info["signups_count_pending"];
 
         $signups_table_size = 0;
 
-        $results = $wpdb->get_results( "SHOW TABLE STATUS LIKE '{$table_name}'", ARRAY_A );
+        $results = $wpdb->get_results( $wpdb->prepare( "SHOW TABLE STATUS LIKE %s", $table_name ), ARRAY_A );
 
         if ( $results ) {
                 $signups_table_size += round((($results[0]['Data_length'] + $results[0]['Index_length']) / 1024 / 1024), 2);
